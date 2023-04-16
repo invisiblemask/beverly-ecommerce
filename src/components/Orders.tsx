d458cc8b772286ac840455e56246b3be2b4483e9
Kelechi Okeke <48678691+invisiblemask@users.noreply.github.com>
2023-04-16 18:46:25 +0100

    created payment method component

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -9,19 +9,19 @@ export default function Orders() {
         <div>Total</div>
       </div>
       <div className="order-list">
-        <div className="id">#2h73h21</div>
+        <div className="id">2h73h21</div>
         <div className="status-p">Pending</div>
         <div className="date">Nov 10, 2022</div>
         <div className="id">£34.0</div>
       </div>
       <div className="order-list">
-        <div className="id">#ffh73h21</div>
+        <div className="id">ffh73h21</div>
         <div className="status-d">Delivered</div>
         <div className="date">Jan 23, 2022</div>
         <div className="id">£14.0</div>
       </div>
       <div className="order-list">
-        <div className="id">#ffi3k90</div>
+        <div className="id">ffi3k90</div>
         <div className="status-c">Cancelled</div>
         <div className="date">Nov 3, 2021</div>
         <div className="id">£62.0</div>
--- src/components/Payments.tsx
+++ src/components/Payments.tsx
@@ -1,3 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
 export default function Payment() {
-  return <div>Payment Method Page</div>;
+  return (
+    <div className="payment">
+      <h2>Payment Method</h2>
+      <div className="payment-list">
+        <div className="card-logo">
+          <img src="/icons/visa.png" alt="" />
+        </div>
+        <div className="card-bearer">Kelechi Okeke</div>
+        <div className="card-number">1234 **** **** ****</div>
+        <div className="card-date">07/25</div>
+        <div className="card-icons">
+          <div className="icon-container">
+            <img src="/icons/pencil.png" alt="" />
+          </div>
+          <div className="icon-container">
+            <img src="/icons/recycle-bin.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="payment-list">
+        <div className="card-logo">
+          <img src="/icons/paypal.png" alt="" />
+        </div>
+        <div className="card-bearer">Kelechi Okeke</div>
+        <div className="card-number">okekekelechi96@gmail.com</div>
+        <div className="card-date">N/A</div>
+        <div className="card-icons">
+          <div className="icon-container">
+            <img src="/icons/pencil.png" alt="" />
+          </div>
+          <div className="icon-container">
+            <img src="/icons/recycle-bin.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
