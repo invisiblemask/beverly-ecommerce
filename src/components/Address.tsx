6140213f6da612fd389b10db873ecfe93f01e088
Kelechi Okeke <48678691+invisiblemask@users.noreply.github.com>
2023-04-16 19:39:49 +0100

    created address component in account page

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -1,3 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
 export default function Address() {
-  return <div>Address Page</div>;
+  return (
+    <div className="address">
+      <h2>Address</h2>
+      <div className="address-list">
+        <div>Nigeria</div>
+        <div>Aliyu Mustapha boulevard wuye Abuja</div>
+        <div>+2347082896308</div>
+        <div className="address-icons">
+          <div className="icon-container">
+            <img src="/icons/pencil.png" alt="" />
+          </div>
+          <div className="icon-container">
+            <img src="/icons/recycle-bin.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="address-list">
+        <div>Romania</div>
+        <div>34 John Crescent Str</div>
+        <div>(555) 555-1234</div>
+        <div className="address-icons">
+          <div className="icon-container">
+            <img src="/icons/pencil.png" alt="" />
+          </div>
+          <div className="icon-container">
+            <img src="/icons/recycle-bin.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
